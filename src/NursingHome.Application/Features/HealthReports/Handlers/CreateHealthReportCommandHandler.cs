fdcb4c1f1ec81e04a972851986f8a24b092036e3
Haoluonghuynhngoc@gmail.com <Haoluonghuynhngoc@gmail.com>
2024-07-04 03:38:33 +0700

    Add Auto Check Status Min Max

--- src/NursingHome.Application/Features/HealthReports/Handlers/CreateHealthReportCommandHandler.cs
+++ src/NursingHome.Application/Features/HealthReports/Handlers/CreateHealthReportCommandHandler.cs
@@ -6,17 +6,37 @@ using NursingHome.Application.Contracts.Repositories;
 using NursingHome.Application.Features.HealthReports.Commands;
 using NursingHome.Application.Models;
 using NursingHome.Domain.Entities;
+using NursingHome.Domain.Enums;
 
 namespace NursingHome.Application.Features.HealthReports.Handlers;
 internal class CreateHealthReportCommandHandler(IUnitOfWork unitOfWork) : IRequestHandler<CreateHealthReportCommand, MessageResponse>
 {
     private readonly IGenericRepository<Elder> _elderRepository = unitOfWork.Repository<Elder>();
     private readonly IGenericRepository<HealthReport> _healthReportRepository = unitOfWork.Repository<HealthReport>();
+    private readonly IGenericRepository<HealthCategory> _healthCategoryRepository = unitOfWork.Repository<HealthCategory>();
+    private readonly IGenericRepository<MeasureUnit> _measureUnitRepository = unitOfWork.Repository<MeasureUnit>();
     public async Task<MessageResponse> Handle(CreateHealthReportCommand request, CancellationToken cancellationToken)
     {
         if (!await _elderRepository.ExistsByAsync(_ => _.Id == request.ElderId, cancellationToken))
         {
             throw new NotFoundException(nameof(Elder), request.ElderId);
+        }
+        foreach (var healthReportDetail in request.HealthReportDetails)
+        {
+            if (!await _healthCategoryRepository.ExistsByAsync(_ => _.Id == healthReportDetail.HealthCategoryId, cancellationToken))
+            {
+                throw new NotFoundException(nameof(HealthCategory), healthReportDetail.HealthCategoryId);
+            }
+            foreach (var healthReportDetailMeasure in healthReportDetail.HealthReportDetailMeasures)
+            {
+                var measureUnit = await _measureUnitRepository.FindByAsync(_
+                    => _.Id == healthReportDetailMeasure.MeasureUnitId)
+                    ?? throw new NotFoundException($"Measure Unit Have Id {healthReportDetailMeasure.MeasureUnitId} Is Not Found");
+
+                healthReportDetailMeasure.Status = measureUnit.MinValue <= healthReportDetailMeasure.Value
+                    && measureUnit.MaxValue >= healthReportDetailMeasure.Value
+                    ? HealthReportDetailMeasureStatus.Normal : HealthReportDetailMeasureStatus.Warning;
+            }
         }
         var healthReport = request.Adapt<HealthReport>();
         await _healthReportRepository.CreateAsync(healthReport, cancellationToken);
--- src/NursingHome.Application/Features/HealthReports/Models/CreateHealthReportDetailMeasureRequest.cs
+++ src/NursingHome.Application/Features/HealthReports/Models/CreateHealthReportDetailMeasureRequest.cs
@@ -1,9 +1,11 @@
 ﻿using NursingHome.Domain.Enums;
+using System.Text.Json.Serialization;
 
 namespace NursingHome.Application.Features.HealthReports.Models;
 public record CreateHealthReportDetailMeasureRequest
 {
     public float Value { get; set; }
+    [JsonIgnore]
     public HealthReportDetailMeasureStatus Status { get; set; }
     public string? Note { get; set; }
     public int MeasureUnitId { get; set; }
