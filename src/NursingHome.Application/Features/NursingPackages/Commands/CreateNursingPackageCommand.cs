0d28771f4fa2f9b1b7c38074f3477ad1007967dc
Haoluonghuynhngoc@gmail.com <Haoluonghuynhngoc@gmail.com>
2024-07-16 16:55:36 +0700

    Update Nursing Package

--- src/NursingHome.Application/Features/NursingPackages/Commands/CreateNursingPackageCommand.cs
+++ src/NursingHome.Application/Features/NursingPackages/Commands/CreateNursingPackageCommand.cs
@@ -9,7 +9,9 @@ public record CreateNursingPackageCommand : IRequest<MessageResponse>
     public string Description { get; set; } = default!;
     public NursingPackageType Type { get; set; } = default!;
     public decimal Price { get; set; }
+    public int NumberOfNurses { get; set; }
     public int RegistrationLimit { get; set; }
     public string? ImageUrl { get; set; }
     public int Capacity { get; set; }
+
 }
