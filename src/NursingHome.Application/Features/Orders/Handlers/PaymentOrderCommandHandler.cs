54ff6cb002abfa28a579a30a94a8292a4df3ab34
Haoluonghuynhngoc@gmail.com <Haoluonghuynhngoc@gmail.com>
2024-07-20 23:40:29 +0700

    Update Payment VnPay

--- src/NursingHome.Application/Features/Orders/Handlers/PaymentOrderCommandHandler.cs
+++ src/NursingHome.Application/Features/Orders/Handlers/PaymentOrderCommandHandler.cs
@@ -63,8 +63,8 @@ internal class PaymentOrderCommandHandler(
             Info = order.Description,
             //PaymentReferenceId = order.Id.ToString(),
             PaymentReferenceId = order.PaymentReferenceId.ToString(),
-            //Time = order.CreatedAt.Value,
-            Time = order.CreatedAt != null ? order.CreatedAt.Value : DateTime.UtcNow,
+            Time = DateTime.UtcNow,
+            //Time = order.CreatedAt != null ? order.CreatedAt.Value : DateTime.UtcNow,
             returnUrl = returnUrl
         });
     }
