abd7ca6b8ef0838004bfaa96ab5a90cc69be5513
Haoluonghuynhngoc@gmail.com <Haoluonghuynhngoc@gmail.com>
2024-07-26 01:36:20 +0700

    Update Care Service Respone

--- src/NursingHome.Application/Features/Elders/Models/ElderCareServiceResponse.cs
+++ src/NursingHome.Application/Features/Elders/Models/ElderCareServiceResponse.cs
@@ -1,7 +1,10 @@
 ﻿using NursingHome.Application.Features.OrderDetails.Models;
+using System.Text.Json.Serialization;
 
 namespace NursingHome.Application.Features.Elders.Models;
 public record ElderCareServiceResponse : BaseElderResponse
 {
+    [JsonIgnore]
     public ICollection<OrderDetailNotElderAndContractResponse> OrderDetails { get; set; } = new HashSet<OrderDetailNotElderAndContractResponse>();
+    public ICollection<OrderDetailNotElderAndContractResponse> OrderDetailsService => OrderDetails.Where(_ => _.ServicePackage != null).ToList();
 }
