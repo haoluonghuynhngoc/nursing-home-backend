7846a10c7daef898c30c81738d93ddf5319c8aab
Haoluonghuynhngoc@gmail.com <Haoluonghuynhngoc@gmail.com>
2024-08-01 01:39:25 +0700

    Update Pagin

--- src/NursingHome.Application/Common/Constants/PaginationConstants.cs
+++ src/NursingHome.Application/Common/Constants/PaginationConstants.cs
@@ -4,7 +4,7 @@ public class PaginationConstants
 {
     public const int DefaultPageIndex = 1;
 
-    public const int DefaultPageSize = 100;
+    public const int DefaultPageSize = 1000;
 
     public const int MaxPageSize = 2000;
 }
