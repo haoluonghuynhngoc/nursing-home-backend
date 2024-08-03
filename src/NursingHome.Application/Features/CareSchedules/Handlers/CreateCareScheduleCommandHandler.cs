b7d7b0f3ac840a8977b5e0497d2d482ed3e3d6e8
Haoluonghuynhngoc@gmail.com <Haoluonghuynhngoc@gmail.com>
2024-08-04 01:34:35 +0700

    Add Notification In Care Schedules

--- src/NursingHome.Application/Features/CareSchedules/Handlers/CreateCareScheduleCommandHandler.cs
+++ src/NursingHome.Application/Features/CareSchedules/Handlers/CreateCareScheduleCommandHandler.cs
@@ -1,17 +1,23 @@
-﻿using Mapster;
+﻿using Hangfire;
+using Mapster;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using NursingHome.Application.Common.Exceptions;
 using NursingHome.Application.Common.Resources;
 using NursingHome.Application.Contracts.Repositories;
+using NursingHome.Application.Contracts.Services.Notifications;
 using NursingHome.Application.Features.CareSchedules.Commands;
 using NursingHome.Application.Models;
+using NursingHome.Application.Models.Notifications;
 using NursingHome.Domain.Entities;
 using NursingHome.Domain.Entities.Identities;
+using NursingHome.Domain.Enums;
+using System.Text.Json;
 
 namespace NursingHome.Application.Features.CareSchedules.Handlers;
 internal sealed class CreateCareScheduleCommandHandler(
-     IUnitOfWork unitOfWork
-    ) : IRequestHandler<CreateCareScheduleCommand, MessageResponse>
+    IUnitOfWork unitOfWork,
+    INotifier notifier) : IRequestHandler<CreateCareScheduleCommand, MessageResponse>
 {
     private readonly IGenericRepository<CareSchedule> _careScheduleRepository = unitOfWork.Repository<CareSchedule>();
     private readonly IGenericRepository<Room> _roomRepository = unitOfWork.Repository<Room>();
@@ -27,7 +33,8 @@ internal sealed class CreateCareScheduleCommandHandler(
         //}
 
         var rooms = await _roomRepository.FindAsync(_ =>
-        request.Rooms.Select(_ => _.Id).Contains(_.Id), isAsNoTracking: false);
+        request.Rooms.Select(_ => _.Id).Contains(_.Id),
+        includeFunc: _ => _.Include(x => x.Elders), isAsNoTracking: false);
 
         // chưa test 
         foreach (var room in rooms)
@@ -54,6 +61,31 @@ internal sealed class CreateCareScheduleCommandHandler(
 
         await _careScheduleRepository.CreateAsync(careSchedules, cancellationToken);
         await unitOfWork.CommitAsync(cancellationToken);
+        foreach (var iteam in request.EmployeeSchedules)
+        {
+            SendNotification(careSchedules.Id, "Thông báo lịch trực",
+                $"Bạn có lịch trực vào tháng {DateTime.Now.AddMonths(1).Month}.  Vui lòng kiểm tra lịch trình của bạn.",
+                iteam.UserId, NotificationLevel.Information, cancellationToken);
+        }
         return new MessageResponse(Resource.CreatedSuccess);
+    }
+
+    private void SendNotification(int id, string title, string content, Guid userId, NotificationLevel notificationLevel, CancellationToken cancellationToken)
+    {
+
+        var notificationMessage = new NotificationRequest
+        {
+            Type = NotificationType.ExpoPush,
+            UserId = userId,
+            Level = notificationLevel,
+            Title = title,
+            Content = content,
+            Data = JsonSerializer.Serialize(new
+            {
+                Id = id,
+                Entity = nameof(CareSchedule)
+            })
+        };
+        BackgroundJob.Enqueue(() => notifier.NotifyAsync(notificationMessage, true, cancellationToken));
     }
 }
