0ec32cb382dd013d6fd0b64eb07470c9dc0f963f
Haoluonghuynhngoc@gmail.com <Haoluonghuynhngoc@gmail.com>
2024-08-18 01:51:35 +0700

    Add State in Desease Category

--- src/NursingHome.Domain/Entities/DiseaseCategory.cs
+++ src/NursingHome.Domain/Entities/DiseaseCategory.cs
@@ -1,8 +1,12 @@
 ﻿using NursingHome.Domain.Common;
+using NursingHome.Domain.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NursingHome.Domain.Entities;
 public class DiseaseCategory : BaseEntity<int>
 {
     public string Name { get; set; } = default!;
+    [Column(TypeName = "nvarchar(24)")]
+    public StateType State { get; set; }
     public virtual ICollection<MedicalRecord> MedicalRecords { get; set; } = new HashSet<MedicalRecord>();
 }
