3f11d96d22e171531e696658e1a8f043094c9e94
Haoluonghuynhngoc@gmail.com <Haoluonghuynhngoc@gmail.com>
2024-08-18 01:49:24 +0700

    Create Api Remove Disease Category

--- src/NursingHome.Application/Features/DiseaseCategories/Queries/GetAllDiseaseCategoryQuery.cs
+++ src/NursingHome.Application/Features/DiseaseCategories/Queries/GetAllDiseaseCategoryQuery.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using NursingHome.Application.Features.DiseaseCategories.Models;
 using NursingHome.Application.Models.Pages;
 using NursingHome.Domain.Entities;
+using NursingHome.Domain.Enums;
 using NursingHome.Shared.Pages;
 using System.Linq.Expressions;
 
@@ -11,10 +12,14 @@ namespace NursingHome.Application.Features.DiseaseCategories.Queries;
 public sealed record GetAllDiseaseCategoryQuery : PaginationRequest<DiseaseCategory>, IRequest<PaginatedResponse<DiseaseCategoryResponse>>
 {
     public string? Search { get; set; }
-
+    public StateType? State { get; set; }
     public override Expression<Func<DiseaseCategory, bool>> GetExpressions()
     {
         Expression = Expression.And(_ => string.IsNullOrWhiteSpace(Search) || EF.Functions.Like(_.Name, $"%{Search}%"));
+        if (State.HasValue)
+        {
+            Expression = Expression.And(_ => _.State == State);
+        }
         return Expression;
     }
 }
