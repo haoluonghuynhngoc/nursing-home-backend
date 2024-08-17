d39f954bc954f66d93d04098f5d057d3d98e883c
Haoluonghuynhngoc@gmail.com <Haoluonghuynhngoc@gmail.com>
2024-08-18 01:52:16 +0700

    Update Api Seed Mail In Api Auto Contract

--- src/NursingHome.Infrastructure/Jobs/TaskSchedulerOrder.cs
+++ src/NursingHome.Infrastructure/Jobs/TaskSchedulerOrder.cs
@@ -3,9 +3,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using NursingHome.Application.Contracts.Jobs;
 using NursingHome.Application.Contracts.Repositories;
+using NursingHome.Application.Contracts.Services;
 using NursingHome.Application.Contracts.Services.Notifications;
 using NursingHome.Application.Models.Notifications;
 using NursingHome.Domain.Entities;
+using NursingHome.Domain.Entities.Identities;
 using NursingHome.Domain.Enums;
 using System.Text.Json;
 
@@ -20,17 +22,25 @@ public class TaskSchedulerOrder : ITaskSchedulerOrder
     private readonly IGenericRepository<ScheduledService> _scheduledServiceRepository;
     private readonly IGenericRepository<Appointment> _appointmentServiceRepository;
     private readonly IGenericRepository<OrderDate> _orderDateRepository;
+    private readonly IGenericRepository<User> _userRepository;
+    private readonly IGenericRepository<Elder> _elderRepository;
+    private readonly IGenericRepository<NursingPackage> _nursingPackageRepository;
+    private readonly IEmailSender _emailSender;
     private readonly INotifier _notifier;
-    public TaskSchedulerOrder(IUnitOfWork unitOfWork, INotifier INotifier, ILogger<TaskSchedulerOrder> logger)
+    public TaskSchedulerOrder(IUnitOfWork unitOfWork, IEmailSender emailSender, INotifier INotifier, ILogger<TaskSchedulerOrder> logger)
     {
         _notifier = INotifier;
+        _emailSender = emailSender;
         _unitOfWork = unitOfWork;
         _contractRepository = unitOfWork.Repository<Contract>();
         _orderRepository = unitOfWork.Repository<Order>();
         _orderDetailRepository = unitOfWork.Repository<OrderDetail>();
         _scheduledServiceRepository = unitOfWork.Repository<ScheduledService>();
         _appointmentServiceRepository = unitOfWork.Repository<Appointment>();
+        _elderRepository = unitOfWork.Repository<Elder>();
+        _nursingPackageRepository = unitOfWork.Repository<NursingPackage>();
         _orderDateRepository = unitOfWork.Repository<OrderDate>();
+        _userRepository = unitOfWork.Repository<User>();
         this.logger = logger;
     }
     public async Task CheckContractExpirationAsync()
@@ -41,41 +51,46 @@ public class TaskSchedulerOrder : ITaskSchedulerOrder
 
             var listContracts = await _contractRepository.FindAsync(
                 expression: _ => _.Status != ContractStatus.Expired && _.Status != ContractStatus.Cancelled);
+            // hơi lỏ khi gửi mail như cũng sài được do là api động
             foreach (var contract in listContracts)
             {
-                if (contract.StartDate == currentDate)
+                if (contract.NursingPackageId != null)
                 {
-                    contract.Status = ContractStatus.Valid;
-                    // gửi mail thông báo hợp đồng đã được kích hoạt
-                    SendNotification(contract.Id, $"Thông Báo Hợp Đồng Ngày {currentDate:dd/MM/yyyy}",
-                       $"Hợp đồng có hiệu lực: Hợp đồng có mã {contract.Name} có hiệu lực vào ngày {currentDate:dd/MM/yyyy}.",
-                     contract.UserId, nameof(Contract), NotificationLevel.Information, CancellationToken.None);
-                }
+                    var elder = await _elderRepository.FindByIdAsync(contract.ElderId);
+                    var user = await _userRepository.FindByIdAsync(contract.UserId);
+                    var nursingPackage = await _nursingPackageRepository.FindByIdAsync(contract.NursingPackageId);
+                    if (elder != null && user != null && nursingPackage != null)
+                    {
+                        if (contract.StartDate == currentDate)
+                        {
+                            contract.Status = ContractStatus.Valid;
+                            SendNotification(contract.Id, $"Thông Báo Hợp Đồng Ngày {currentDate:dd/MM/yyyy}",
+                               $"Hợp đồng có hiệu lực: Hợp đồng có mã {contract.Name} có hiệu lực vào ngày {currentDate:dd/MM/yyyy}.",
+                             contract.UserId, nameof(Contract), NotificationLevel.Information, CancellationToken.None);
+                            await SendMail(contract, elder, nursingPackage, user, contract.StartDate, "#4CAF50", "Hợp Đồng Đã Có Hiệu Lực");
+                        }
 
-                var notificationDate = contract.EndDate;
-                if (notificationDate.AddDays(-30) == currentDate)
-                {
-                    // gửi email thông báo sắp hết hạn
-                    SendNotification(contract.Id, $"Thông Báo Hợp Đồng Ngày {currentDate:dd/MM/yyyy}",
-                      $"Hợp đồng sắp hết hạn (cách 30 ngày): Bạn có muốn gia hạn hợp đồng có mã {contract.Name} sẽ hết hạn vào ngày {contract.EndDate:dd/MM/yyyy}",
-                    contract.UserId, nameof(Contract), NotificationLevel.Information, CancellationToken.None);
-                }
-                if (contract.EndDate <= currentDate)
-                {
-                    contract.Status = ContractStatus.Expired;
-                    // gửi email thông báo hợp đồng đã hết hạn
-                    SendNotification(contract.Id, $"Thông Báo Hợp Đồng Ngày {currentDate:dd/MM/yyyy}",
-                     $"Hợp đồng có mã {contract.Name} đã hết hạn vào ngày{currentDate:dd/MM/yyyy}.",
-                   contract.UserId, nameof(Contract), NotificationLevel.Information, CancellationToken.None);
-                }
+                        var notificationDate = contract.EndDate;
+                        if (notificationDate.AddDays(-30) == currentDate)
+                        {
+                            SendNotification(contract.Id, $"Thông Báo Hợp Đồng Ngày {currentDate:dd/MM/yyyy}",
+                              $"Hợp đồng sắp hết hạn (cách 30 ngày): Bạn có muốn gia hạn hợp đồng có mã {contract.Name} sẽ hết hạn vào ngày {contract.EndDate:dd/MM/yyyy}",
+                            contract.UserId, nameof(Contract), NotificationLevel.Information, CancellationToken.None);
+                            await SendMail(contract, elder, nursingPackage, user, contract.StartDate, "#FF9800", "Hợp Đồng Sắp Hết Hạn");
+                        }
+                        if (contract.EndDate <= currentDate)
+                        {
+                            contract.Status = ContractStatus.Expired;
+                            SendNotification(contract.Id, $"Thông Báo Hợp Đồng Ngày {currentDate:dd/MM/yyyy}",
+                             $"Hợp đồng có mã {contract.Name} đã hết hạn vào ngày{currentDate:dd/MM/yyyy}.",
+                           contract.UserId, nameof(Contract), NotificationLevel.Information, CancellationToken.None);
+                            await SendMail(contract, elder, nursingPackage, user, contract.StartDate, "#F44336", "Hợp Đồng Đã Hết Hạn");
+                        }
+                        await _contractRepository.UpdateAsync(contract);
+                        await _unitOfWork.CommitAsync();
+                    }
 
-                //if (contract.StartDate < currentDate && contract.EndDate > currentDate)
-                //{
-                //    // thực hiện 1 vài cái gì đó 
-                //}
-
-                await _contractRepository.UpdateAsync(contract);
-                await _unitOfWork.CommitAsync();
+                }
             }
         }
         catch (Exception ex)
@@ -83,6 +98,128 @@ public class TaskSchedulerOrder : ITaskSchedulerOrder
             logger.LogError(ex, "An error occurred while checking contract expiration.");
         }
     }
+    private async Task SendMail(Contract contract, Elder elder, NursingPackage nursingPackage, User user, DateOnly date, string color, string title)
+    {
+        try
+        {
+            var content = contract.Status switch
+            {
+                ContractStatus.Valid => "Hợp đồng đã có hiệu lực",
+                ContractStatus.Pending => "Hợp đồng đang chờ xác nhận",
+                ContractStatus.Expired => "Hợp đồng đã hết hạn",
+                ContractStatus.Cancelled => "Hợp đồng đã bị hủy",
+                _ => "Hợp đồng không xác định"
+            };
+
+            var emailSubject = $"Thông báo về tình trạng hợp đồng ngày {date:dd/MM/yyyy}";
+            var emailBody = $@"<!DOCTYPE html>
+           <html lang='vi'>
+           <head>
+               <meta charset='UTF-8'>
+               <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+               <title>Thông Báo Tình Trạng Hợp Đồng: {contract.Name}</title>
+               <style>
+                   body {{
+                       font-family: Arial, sans-serif;
+                       line-height: 1.6;
+                       color: #333;
+                       background-color: #f4f4f4;
+                       margin: 0;
+                       padding: 0;
+                   }}
+                   .container {{
+                       width: 100%;
+                       max-width: 600px;
+                       margin: 30px auto;
+                       padding: 20px;
+                       border: 1px solid #ddd;
+                       border-radius: 10px;
+                       background-color: #fff;
+                       box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                   }}
+                   h2 {{
+                       color: {color};
+                       font-size: 28px;
+                       text-align: center; 
+                       margin-bottom: 20px;
+                   }}
+                   p {{
+                       font-size: 16px;
+                       margin-bottom: 10px;
+                   }}
+                   ul {{
+                       list-style-type: none;
+                       padding: 0;
+                       margin: 0;
+                   }}
+                   ul li {{
+                       padding: 10px;
+                       margin-bottom: 10px;
+                       background: #f9f9f9;
+                       border-left: 4px solid {color};
+                       border-radius: 5px;
+                       font-size: 15px;
+                   }}
+                   ul li strong {{
+                       display: inline-block;
+                       width: 150px;
+                       font-weight: bold;
+                   }}
+                   .footer {{
+                       margin-top: 30px;
+                       text-align: center;
+                       font-size: 14px;
+                       color: #888;
+                   }}
+                   .footer img {{
+                       margin-top: 20px;
+                       max-width: 100%;
+                       border-radius: 10px;
+                       box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+                   }}
+               </style>
+           </head>
+           <body>
+               <div class='container'>
+                   <h2>{title}</h2>
+                   <p>Kính gửi {user.FullName},</p>
+                   <p>Chúng tôi xin thông báo tình trạng hợp đồng của bạn như sau:</p>
+        
+                   <h3>Chi Tiết Hợp Đồng</h3>
+                   <ul>
+                       <li><strong>Tên Hợp Đồng:</strong> {contract.Name}</li>
+                       <li><strong>Tên Người Cao Tuổi:</strong> {elder.Name}</li>
+                       <li><strong>Tên Người Giám Hộ:</strong> {user.FullName}</li>
+                       <li><strong>Gói Dưỡng Lão:</strong> {nursingPackage.Name}</li>
+                       <li><strong>Trạng Thái:</strong> {content}</li>
+                       <li><strong>Ngày Ký:</strong> {contract.SigningDate:dd-MM-yyyy}</li>
+                       <li><strong>Ngày Có Hiệu Lực:</strong> {contract.StartDate:dd-MM-yyyy}</li>
+                       <li><strong>Ngày Hết Hạn:</strong> {contract.EndDate:dd-MM-yyyy}</li>
+                       <li><strong>Ghi Chú:</strong> {contract.Notes}</li>
+                   </ul>
+
+                   <h3>Trạng Thái Hợp Đồng</h3>
+                   <p>{content}</p>
+
+                   <div class='footer'>
+                       <p>Xin cảm ơn bạn đã tin tưởng và sử dụng dịch vụ của chúng tôi.</p>
+                       <img src='https://firebasestorage.googleapis.com/v0/b/careconnect-2d494.appspot.com/o/images%2Ff4dbf5a6-1495-4314-9e64-52e35d2564fd.png?alt=media&token=926dd20b-667a-431d-a1a7-dc7fec3243e5' alt='Contract Status Image'>
+                   </div>
+               </div>
+           </body>
+           </html>";
+
+            if (user.Email != null)
+            {
+                await _emailSender.SendEmailAsync(user.Email, emailSubject, emailBody);
+            }
+        }
+        catch (Exception ex)
+        {
+            // Xử lý ngoại lệ tại đây, ví dụ: ghi log hoặc thông báo lỗi
+        }
+    }
+
     public async Task CheckOrderExpirationAsync()
     {
         try
